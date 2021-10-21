19035ef2ebf7153d57b1c87460f9dce49af36610
darrenrector91 <darrenrector91@gmail.com>
2021-10-21 15:27:08 -0500

    added modal calc

--- scripts/script.js
+++ scripts/script.js
@@ -354,7 +354,7 @@ $("#pkgModalCloseBtn").on("click", function () {
 const buildPkgModal = (arr) => {
   let localPkgName = localStorage.getItem("packageName");
   $("#pkgModal").modal("show");
-  modalPkgTitle.innerHTML = `<h6>Package name :</h6> ${localPkgName} `;
+  modalPkgTitle.innerHTML = `<h6>Package name:</h6> ${localPkgName} `;
   let matList = resArr[0];
 
   let modalDisplayPkg = matList.filter((item) => arr.includes(item.mId));
